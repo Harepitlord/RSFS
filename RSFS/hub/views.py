f8aaf3c30703c9135bcfe1bda9d5ed16b444cd84
Hareeswaran O S <65818462+Harepitlord@users.noreply.github.com>
2023-02-09 22:47:57 +0530

    Fixed an issue in About.html

--- RSFS/hub/views.py
+++ RSFS/hub/views.py
@@ -1,21 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.handlers.wsgi import WSGIRequest
 from django.views import View
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect, render
 
 
 class Home(View):
-    def get(self, request):
+    def get(self, request:WSGIRequest):
         return render(request=request, template_name="hub/home.html")
 
 
 class DashBoard(LoginRequiredMixin, View):
     login_url = reverse_lazy('UserManagement:Login')
 
-    def get(self, request):
+    def get(self, request:WSGIRequest):
         return render(request=request, template_name="hub/Dashboard.html")
 
 
 class About(View):
-    def get(self, request):
-        return render(request=request, template_name='hub/About.html')
+    def get(self, request:WSGIRequest):
+        context = {}
+        if request.user.is_authenticated:
+            context['base'] = 'RSFS/LoginBase.html'
+        else:
+            context['base'] = 'RSFS/NonLoginBase.html'
+        return render(request=request, template_name='hub/About.html',context=context)
